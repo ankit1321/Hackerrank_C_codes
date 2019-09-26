209f46261bd78870c00e655cef251b05d6a5fef9
Ankit prasad <ankitprasad3948@gmail.com>
2019-09-26 14:10:02 +0530

    added boxes through tunnel code

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ In this program bitwise "AND(&)",bitwise "OR(|)" and bitwise "XOR(^)" functions 
 #SmallTriangles_LargeTriangles
 
 In this program triangles are  sorted according to the size of perimeter where all sides of triangles are given.And then printed again sides of triangle in sorted way according to the perimeter.
+
+#Boxes_through_tunnel
+
+You are transporting some boxes through a tunnel, where each box is a parallelepiped, and is characterized by its length, width and height.
+
+The height of the tunnel 41 feet and the width can be assumed to be infinite. A box can be carried through the tunnel only if its height is strictly less than the tunnel's height. Find the volume of each box that can be successfully transported to the other end of the tunnel. Note: Boxes cannot be rotated.
+Code is written in C.
---
+++ boxes_through_tunnel.c
@@ -0,0 +1,52 @@
+/*Contributer Name:Ankit Prasad
+  Date:15th september  2019
+  All copyrights: www.aptechnoservices.com © 2019
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+	int length,width,height;
+    /**
+	* Define three fields of type int: length, width and height
+	*/
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+    return (b.height*b.length*b.width);
+	/**
+	* Return the volume of the box
+	*/
+}
+
+int is_lower_than_max_height(box b) {
+    if(b.height<MAX_HEIGHT)
+        return 1;
+    return 0;
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	box *boxes = malloc(n * sizeof(box));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+	}
+	for (int i = 0; i < n; i++) {
+		if (is_lower_than_max_height(boxes[i])) {
+			printf("%d\n", get_volume(boxes[i]));
+		}
+	}
+	return 0;
+}
